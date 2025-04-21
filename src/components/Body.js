0ab0ee64fd09bb11708f348ee3b7583ac4d03ace
Ayush Jain <ayushjain8719@gmail.com>
2025-04-22 03:04:25 +0530

    added useState()

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import RestaurantCards from "./RestaurantCards";
 import { mockResData } from "../mocks/mockResData";
 
 const Body = () => {
+  const [filterRestaurants, setFilterRestaurants] = useState(mockResData);
+  const handleFilter = () => {
+    // console.log("click");
+    setFilterRestaurants(
+      filterRestaurants.filter((res) => res?.card?.card?.info?.avgRating > 4)
+    );
+  };
+
+  const handleReset = () => {
+    setFilterRestaurants(mockResData);
+  };
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      {/* <div className="search">Search</div> */}
+      <div>
+        <button
+          className="filter-btn"
+          onClick={handleFilter}
+          disabled={filterRestaurants !== mockResData}
+        >
+          Top Rated Restaurants near You
+        </button>
+        <button
+          className="filter-btn"
+          style={{ marginLeft: "1rem" }}
+          onClick={handleReset}
+          disabled={filterRestaurants === mockResData}
+        >
+          Reset
+        </button>
+      </div>
       <div className="res-container">
-        {mockResData.map((resInfo) => {
+        {filterRestaurants.map((resInfo) => {
           return (
             <RestaurantCards
               key={resInfo.card.card.info.id}
